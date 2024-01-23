09fc38657d9376c0c23fdc1b4ea1d987b57bab0a
Simon A.D. Winter <112318684+SimonADW@users.noreply.github.com>
2024-01-23 15:15:22 +0100

    Create js module

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 
+import renderCard from "./renderCard.js";
 
 function CreateFilmCard(title, director, releaseYear, episodeID, openingCrawl, imageUrl) {
 	this.Title = title,
@@ -6,7 +7,7 @@ function CreateFilmCard(title, director, releaseYear, episodeID, openingCrawl, i
 	this.ReleaseYear = releaseYear, 
 	this.EpisodeID = episodeID, 
 	this.OpeningCrawl = openingCrawl,
-	this.ImageUrl = imageUrl
+	this.ImageUrl = imageUrl 
 }
 
 const attackOfTheClones = new CreateFilmCard(
@@ -35,56 +36,6 @@ const empireStrikesBack = new CreateFilmCard(
 	"It is a dark time for the Rebellion.\r\nAlthough the Death Star has been\r\ndestroyed, Imperial troops have\r\ndriven the Rebel forces from their\r\nhidden base and pursued them\r\nacross the galaxy.\r\n\r\nEvading the dreaded Imperial\r\nStarfleet, a group of freedom\r\nfighters led by Luke Skywalker has\r\nestablished a new secret base on\r\nthe remote ice world of Hoth.",
 	"assets/images/films/empireStrikesBack.jpeg"
 );
-
-
-const renderCard = (objectToRender)=> {
-	const cardContainer = document.querySelector(".cards");
-	
-	const card = document.createElement("div");
-	card.classList.add("cards__card");
-	cardContainer.appendChild(card);
-	
-	const imageContainer = document.createElement("img");
-	imageContainer.classList.add("card__image");
-	imageContainer.setAttribute("src", objectToRender.ImageUrl)
-	
-	const cardContent = document.createElement("div");
-	cardContent.classList.add("card__content");
-
-	const cardTitle = document.createElement("h5");
-	cardTitle.textContent = objectToRender.Title;
-	cardContent.appendChild(cardTitle);
-	
-	card.append(imageContainer, cardContent);
-
-	const cardContentList = document.createElement("ul");
-	
-	cardContent.append(cardTitle, cardContentList)
-	
-	for (let key in objectToRender) {
-		if (key === "Title" || key === "ImageUrl") {
-			continue;
-		}	
-				
-		const listItem = document.createElement("li");
-		cardContentList.appendChild(listItem);
-
-		const keySpan = document.createElement("span");
-		keySpan.textContent = `${key}: `;
-		const valueSpan = document.createElement("span");
-		valueSpan.textContent = objectToRender[key];		
-		listItem.append(keySpan, valueSpan);
-	}
-
-	const lightSaber = document.createElement("div");
-	lightSaber.classList.add("light-saber");
-	card.appendChild(lightSaber);
-
-	const saberHandle = document.createElement("div");
-	const saberLight = document.createElement("div");
-	lightSaber.append(saberHandle, saberLight);
-}
-
 
 renderCard(attackOfTheClones);
 renderCard(aNewHope);
---
+++ src/js/renderCard.js
@@ -0,0 +1,49 @@
+const renderCard = (objectToRender)=> {
+	const cardContainer = document.querySelector(".cards");
+	
+	const card = document.createElement("div");
+	card.classList.add("cards__card");
+	cardContainer.appendChild(card);
+	
+	const imageContainer = document.createElement("img");
+	imageContainer.classList.add("card__image");
+	imageContainer.setAttribute("src", objectToRender.ImageUrl)
+	
+	const cardContent = document.createElement("div");
+	cardContent.classList.add("card__content");
+
+	const cardTitle = document.createElement("h5");
+	cardTitle.textContent = objectToRender.Title;
+	cardContent.appendChild(cardTitle);
+	
+	card.append(imageContainer, cardContent);
+
+	const cardContentList = document.createElement("ul");
+	
+	cardContent.append(cardTitle, cardContentList)
+	
+	for (let key in objectToRender) {
+		if (key === "Title" || key === "ImageUrl") {
+			continue;
+		}	
+				
+		const listItem = document.createElement("li");
+		cardContentList.appendChild(listItem);
+
+		const keySpan = document.createElement("span");
+		keySpan.textContent = `${key}: `;
+		const valueSpan = document.createElement("span");
+		valueSpan.textContent = objectToRender[key];		
+		listItem.append(keySpan, valueSpan);
+	}
+
+	const lightSaber = document.createElement("div");
+	lightSaber.classList.add("light-saber");
+	card.appendChild(lightSaber);
+
+	const saberHandle = document.createElement("div");
+	const saberLight = document.createElement("div");
+	lightSaber.append(saberHandle, saberLight);
+};
+
+export default renderCard; 
