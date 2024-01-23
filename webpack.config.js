86545ae39b594517186c12d01a8e81b60a9c9162
Simon A.D. Winter <112318684+SimonADW@users.noreply.github.com>
2024-01-23 19:36:41 +0100

    Bundle production

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,9 @@
-// const path = require("path")
 
-// module.exports = {
-// 	mode: "development", 
-// 	entry: "./src/js/main.js,",
-// 	output: {
-// 		path: path.resolve(__dirname, "dist"),
-// 		filename: "bundle.js"
-// 	},
-// 	module: {
-// 		rules: [
-// 			{
-// 				test: /\.(js)$/,
-// 				exclude: /node_modules/,
-// 				use: { 
-// 					loader: "babel-loader",
-// 					options: {
-// 						presets: [
-// 							["@babel/preset-env"]
-// 						]
-// 					}
-// 				}
-// 			}
-// 		]
-// 	},
-// watch: true
-// }
 
 
 const path = require('path')
 module.exports = {
-mode: 'development',
+mode: 'production',
 entry: './src/js/main.js',
 output: {
 path: path.resolve(__dirname,'dist'),
